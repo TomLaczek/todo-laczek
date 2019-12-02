960729cbbbc19e563825dc5faf05d4b0b67a7602
ToM <tomaszlaczek@hotmail.com>
2019-12-02 23:28:02 +0100

    more data in form added simple validation

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -8,27 +8,58 @@
                 Add new project
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3">
-                    <v-text-field label="Title" v-model="title" prepend-inner-icon="edit"></v-text-field>
-                    <v-textarea label="Information" v-model="content"  prepend-inner-icon="edit"></v-textarea>
-                    <v-btn left @click="submit" class="my-3 accent">submit</v-btn>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="Title" v-model="title" :rules="titleRules" prepend-icon="edit"></v-text-field>
+                    <v-textarea label="Information" v-model="content" :rules="contentRules" prepend-icon="edit"></v-textarea>
+                    <v-menu 
+                        v-model="datePicker"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        :nudge-right="40"
+                        min-width="290px"
+                    >
+                        <template v-slot:activator="{ on }">
+                            <v-text-field
+                                readonly 
+                                v-on="on" 
+                                label="Due by" 
+                                v-model="date" 
+                                prepend-icon="event"
+                            ></v-text-field>
+                        </template>
+                            <v-date-picker v-model="date" @input="datePicker = false"></v-date-picker>
+                    </v-menu>
+                    <v-btn @click="submit" class="my-3 accent text-center">submit</v-btn>
                 </v-form>
             </v-card-text>
         </v-card>
     </v-dialog>
 </template>
 <script>
+
 export default {
     data(){
         return{
             title:'',
             content:'',
+            date: new Date().toISOString().substr(0,10),
+            datePicker: false,
+            titleRules: [
+                v => !!v || 'Title is required',
+                v => (v && v.length <= 15) || 'Title must be less than 15 characters',
+            ],
+            contentRules: [
+                v => !!v || 'Information is required',
+                v => (v && v.length > 10) || 'Information must be have more than 10 characters',
+            ],
+
         };
     },
     methods:{
         submit () {
-            alert(this.title + '\n'+ this.content);
-      },
-    }
+            if(this.$refs.form.validate()) alert(this.title + '\n'+ this.content);
+        }
+    },
 }
 </script>
